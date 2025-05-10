11b56a350d244dff0214bf4fe77faa233969567c
prime_earth_realty <developer@primeearthrealty.com>
2025-05-11 00:06:57 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,11 +8,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/prime_earth_logo-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/prime_earth_logo-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/prime_earth_logo-180x180.png" />
+        <title>Prime Earth Realty</title>
+        <meta name="description" content="Real Estate" />
+      </head>
       <body className="antialiased transition-colors duration-200">
-      
         <Header />
         {children}
-        
       </body>
     </html>
   );
