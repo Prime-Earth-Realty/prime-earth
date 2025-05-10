1406b2b503603cdb97a17e5626039edc8cf61778
prime_earth_realty <developer@primeearthrealty.com>
2025-05-10 23:41:42 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./app/**/*.{js,jsx,ts,tsx}",
-      "./components/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          sans: ['var(--font-geist-sans)'],
-          mono: ['var(--font-geist-mono)'],
-        },
+  darkMode: 'class',
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-geist-sans)'],
+        mono: ['var(--font-geist-mono)'],
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
