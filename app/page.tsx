6a006b4d63705021db674eb0798a0e249517672b
prime_earth_realty <developer@primeearthrealty.com>
2025-05-10 23:36:14 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Header from '@/app/components/Header';
 import Hero from '@/app/components/Hero';
 import Section from '@/app/components/Section';
 import ContactForm from '@/app/components/ContactForm';
@@ -12,26 +11,15 @@ import AboutUs from '@/app/components/AboutUs'
 export default function Home() {
   return (
     <>
-      <Header />
       <Hero />
-      
-     <AboutUs />
-
-     <Investment />
-
-
-      
-      
+      <AboutUs />
+      <Investment />
       <Plots />
-      
       <Testimonials />
-      
       <FAQ />
-      
       <Section id="contact" title="Contact Us" subtitle="We'd love to hear from you!">
         <ContactForm />
       </Section>
-      
       <Footer />
     </>
   );
